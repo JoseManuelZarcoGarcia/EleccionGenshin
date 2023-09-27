5c0ae5c72d2f4b9ac344c55025ecf8f57ffb0f8a
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-09-27 11:34:19 +0200

    Agregar archivos de proyecto.

---
+++ Servicios/EleccionGenshinInterfaz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleccionGenshin.Servicios
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal interface EleccionGenshinInterfaz
+    {
+        /// <summary>
+        /// Metodo que implementara por consola el menu
+        /// </summary>
+        public void implementacionMenu();
+
+    }
+}
